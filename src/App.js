262b376602985081c7133d6822427caf96d1c17c
Tamilselvan1255 <tamilselvan0677@gmail.com>
2024-08-27 10:19:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import constructionGif from './assets/Under construction-pana.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className="text-center" style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Row className="justify-content-center align-items-center">
+          <Col xs={12} md={8} lg={6}>
+            <img 
+              src={constructionGif} 
+              alt="Under Construction" 
+              style={{ width: "100%", height: "auto", maxWidth: "450px", maxHeight: "300px" }}  
+            />
+            <h1>Our site is <b>UNDER CONSTRUCTION</b></h1>
+            <p>Please contact us at <b>+919788825633</b> for further information.</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
